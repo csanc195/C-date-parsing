c578196d195dc8815104bbf71f126b35a78c499f
Carlos <csanc195@fiu.edu>
2016-12-31 23:43:20 -0500

    Update findDates.c

--- findDates.c
+++ findDates.c
@@ -1,32 +1,31 @@
 /***************************************************************************/
-/*      findDates.c 	 Prog I 										   */
+/*      findDates.c 	 Prog I					           */
 /***************************************************************************/
-/* Author: Carlos L Sanchez Vila   Panther ID: 5446178					   */
-/* Class: COP4338, Instructor: William Feilds Session: Tu and Th 6:25 pm   */
+/* Author: Carlos L Sanchez Vila   				           */
 /***************************************************************************/
-/*                      Disclaimer:										   */
+/*                      Disclaimer:					   */
 /* I hereby certify that this collective work is my own and none of it is  */
-/* the work of any other person or entity.								   */
+/* the work of any other person or entity.				   */
 /***************************************************************************/
-/*              		Program Description:							   */
+/*              		Program Description:			   */
 /* The following program goes through imput passed through stdin and 	   */
-/* reads entries that follow the pattern of 							   */
-/*			number(month)/number(day)/number(year)						   */
+/* reads entries that follow the pattern of 				   */
+/*			number(month)/number(day)/number(year)		   */
 /* verifies that they represent a valid date, that is, the date is valid   */
 /* if the day is a number from 1 to 31, if a month is a number from 1 to 12*/
 /* and if the year is less or equal to INT_MAX which is the largest int    */
 /* number that can be expressed in c, integers range from -2^32 to (2^32)-1*/
 /* The date is then tested for accuracy, that is if the date is correct and*/
-/* and measured with leap year accuracy.								   */
+/* and measured with leap year accuracy.				   */
 /***************************************************************************/
-/* 						Instructions/How to use:						   */
+/* 			Instructions/How to use:	   		   */
 /* To compile and run this specific program just enter:	                   */
-/* 		make build1														   */
-/* followed by: 														   */
-/*      ./dateParser.out num < filename									   */
+/* 		make build1						   */
+/* followed by: 							   */
+/*      ./dateParser.out num < filename					   */
 /* where num is the number of valid dates that the user desires to parse   */
-/* and filename is the file to parse.							           */
-/* 																		   */
+/* and filename is the file to parse.					   */
+/* 									   */
 /***************************************************************************/
 #include "findDates.h"	/* Include the header file for the program findDates*/
 
